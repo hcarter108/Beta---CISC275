aba67013e297b409f59f99c736fdad1e3b6225bb
hcarter <hcarter@ECALC-SPL-22.win.udel.edu>
2017-12-08 15:20:13 -0500

    JUnit tests done

--- src/tests/TestMovingVector.java
+++ src/tests/TestMovingVector.java
@@ -1,5 +1,15 @@
 package tests;
 
+import org.junit.Test;
+
+import model.MovingVector;
+
 public class TestMovingVector {
 
+	@Test
+	public void testGetterAndSetter(){
+		MovingVector mv =  new MovingVector(1,1);
+		assert(mv.getX()==1);
+		assert(mv.getY()==1);
+	}
 }
--- src/tests/TestPlayer.java
+++ src/tests/TestPlayer.java
@@ -1,5 +1,34 @@
 package tests;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Coordinates;
+import model.MoveableObject;
+import model.Player;
+import model.MoveableObject.MoveableType;
+import model.MovingVector;
+import view.ScreenPanel.Screens;
+
 public class TestPlayer {
 
+	Player p;
+	
+	@Before
+	public void setUpPlayer(){
+		p= new Player(new Coordinates(100,100), Screens.L1);
+	}
+	
+	@Test
+	public void testDiameter(){
+		assert(p.getPlayerDiameter()==new MoveableObject().calculateMinDiameter(new MoveableObject()
+				.getMoveableDimensions(Screens.L1, MoveableType.PLAYER)));
+	}
+	
+	@Test
+	public void testMoveByVector(){
+		assert(p.getCoordinates().getxPos()==100 && p.getCoordinates().getyPos()==100);
+		p.moveByVector(new MovingVector(10,10));
+		assert(p.getCoordinates().getxPos()==110 && p.getCoordinates().getyPos()==110);
+	}
 }
